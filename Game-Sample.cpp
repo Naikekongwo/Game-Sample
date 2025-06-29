29f8a281350165747984eb9eac116b706a160bfa
Naikekongwo <2864364583@qq.com>
2025-06-29 23:46:22 +0800

    转为使用CMAKE部署

---
+++ Game-Sample.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "engine/opencore.h"
+
+using namespace std;
+
+
+int WinMain(int argc, char** args)
+{
+    openCore core;
+    try
+    {
+        core.Start();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        system("pause");
+    }
+    
+    return EXIT_SUCCESS;
+}
---
+++ note/ECLIPSEA 整体设计.md
@@ -0,0 +1,15 @@
+# ECLIPSEA 无水之境 设计大纲
+
+*2025年4月24日 刘绍阳*
+
+----
+
+### 一、文件结构
+
+>/ Game-Sample Project <br>
+|---- assets/ 资源文件夹 <br>
+|---- engine/ 引擎源代码文件夹 <br>
+|---- maps/ 地图文件夹 <br>
+|---- stage/ 场景文件夹 <br>
+|---- Game-Sample.cpp 游戏主源码（编译后为ECLIPSEA.exe）
+
