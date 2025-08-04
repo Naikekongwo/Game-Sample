d0ad77ea7a3d762e15debf75060cb19ed385c65e
shalltyzhuchang <149221088+shalltyzhuchang@users.noreply.github.com>
2025-08-04 15:47:15 +0800

    解决线程阻塞问题
    解决线程阻塞问题

--- include/OpenCore/ResourceManager.h
+++ include/OpenCore/ResourceManager.h
@@ -1,6 +1,7 @@
 #ifndef _RESOURCE_MANAGER_H_
 #define _RESOURCE_MANAGER_H_
 
+
 #include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL.h>
 
@@ -12,37 +13,28 @@
 #include <queue>
 #include <condition_variable>
 #include <thread>
+#include <exception>
 
-
-struct TextureLoadTask
-{
+struct TextureLoadTask {
     bool isFinished = false;
     std::string path;
     std::future<void> future;
     short id;
 };
 
-
-
-// 事关储存资源的对应的智能指针的对应销毁方法
 struct SDLDeleter {
     void operator()(Mix_Music* music) const;
     void operator()(SDL_Texture* texture) const;
 };
 
-// 使用智能指针来储存对应的指针，并且都自定义了对应的析构函数（Deleter）
 using MusicPtr = std::unique_ptr<Mix_Music, SDLDeleter>;
 using TexturePtr = std::unique_ptr<SDL_Texture, SDLDeleter>;
 
-
-// 资源管理器类
 class ResourceManager {
 public:
-    // 单例
     static ResourceManager& getInstance();
 
     bool Init(SDL_Renderer* render);
-
     void CleanUp();
 
     void LoadMusic(short id, const std::string& path);
@@ -55,32 +47,39 @@ public:
     std::future<void> LoadTextureAsync(short id, const std::string &path);
 
     void ClearAll();    
+    void ProcessMainThreadTasks(); // 新增：主线程任务处理
 
 private:
-
-    SDL_Renderer* renderer; // 渲染器指针
+    SDL_Renderer* renderer = nullptr;
 
     void StartWorker();
-    void StopQueue();
+    void StopWorker();
 
-    // 开始任务的模板
     template<typename F>
     std::future<void> EnqueueTask(F&& f);
 
-    // 音乐锁和储存音乐的无序图
+    // 主线程任务队列
+    std::mutex mainThreadQueueMutex_;
+    std::queue<std::function<void()>> mainThreadTaskQueue_;
+
+    // 新增测试：加载表面和纹理转换,这个正常不应该在此类中实现
+    SDL_Surface* LoadSurface(const std::string& path);
+    void ConvertSurfaceToTexture(short id, SDL_Surface* surface);
+
+    // 资源缓存
     std::mutex musicMutex_;
     std::unordered_map<short, MusicPtr> musicCache_;
 
-    // 纹理锁和储存纹理的无序图
     std::mutex textureMutex_;
     std::unordered_map<short, TexturePtr> textureCache_;
 
+    // 工作线程
     std::thread worker_;
-    std::atomic<int> ActiveTask_{0};
+    std::atomic<int> activeTasks_{0};
     std::mutex queueMutex_;
     std::condition_variable queueCV_;
     std::queue<std::function<void()>> taskQueue_;
-    bool shouldStop_ = false;
+    std::atomic<bool> shouldStop_{false};
 };
 
 template<typename F>
@@ -97,4 +96,4 @@ std::future<void> ResourceManager::EnqueueTask(F&& f) {
     return task->get_future();
 }
 
-#endif // _RESOURCE_MANAGER_H_
+#endif //_RESOURCE_MANAGER_H_
