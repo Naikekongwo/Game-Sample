ccd3f7364d839e55d9fe8dbedb5d1f72bc993ea6
shalltyzhuchang <149221088+shalltyzhuchang@users.noreply.github.com>
2025-10-24 05:13:20 +0800

    Add files via upload

--- include/OpenCore/Stage/StageController.hpp
+++ include/OpenCore/Stage/StageController.hpp
@@ -13,22 +13,24 @@
 #include <vector>
 #include <unordered_map>
 
+enum index{root,overlay,top};
+
 class StageController {
     public:
     ~StageController() = default;
 
-    // 切换场景
-    short changeStage(std::unique_ptr<Stage> newStage);
-
+    // 切换场景，默认为root；
+    void changeStage(std::unique_ptr<Stage> newStage);
     //通过id切换场景
-    bool changeStage(short id);
-
-    //在场景池中加入场景
-    short addStageToCache(std::unique_ptr<Stage> newStage,short id);
-
-    //删除对应id的场景
-    bool deleteStage(short id);
-
+    void changeStage(std::unique_ptr<Stage> newStage,int index);
+    //加入临时场景（这里是指比如说鼠标悬停在什么上会弹出一个说明之类的）
+    void addTempStage(std::unique_ptr<Stage> newStage);
+    //删除前三个场景
+    bool deleteStage(int index);
+    //删除最后一个临时场景
+    bool deleteTempStage();
+    //交换前三个场景,比如拖到物品时其图层可能在菜单图标之上，此时需要暂时交换图层
+    void swapStage(int index1,int index2);
     // 处理事件的公共接口
     bool handlEvents(SDL_Event* event);
 
@@ -39,9 +41,7 @@ class StageController {
     void onRender();
     
     private:
-    std::unique_ptr<Stage> currentStage; // 当前场景
-    short currentId;
-    std::unordered_map<short,std::unique_ptr<Stage>> stageCache_;//场景池,且场景池中short不应为0和-1，0、-1用于做信号量
+    std::vector <std::unique_ptr<Stage>> currentStages; // 当前场景,大体上是这样的，强三层为固有的（可以为空），后面可以加临时场景
 };
 
 #endif //_STAGECONTROLLER_H_
